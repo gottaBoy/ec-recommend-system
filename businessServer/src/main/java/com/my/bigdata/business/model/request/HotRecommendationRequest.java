701b77f3c7ea93f5abb85ef65dce941de1515ac7
gottaBoy <460632205@qq.com>
2022-12-23 15:42:03 +0800

    bigdata for recommend system

---
+++ businessServer/src/main/java/com/my/bigdata/business/model/request/HotRecommendationRequest.java
@@ -0,0 +1,18 @@
+package com.my.bigdata.business.model.request;
+
+public class HotRecommendationRequest {
+
+    private int sum;
+
+    public HotRecommendationRequest(int sum) {
+        this.sum = sum;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
